fe69bde66d6e952796354f070b46bdf652e7cd9d
Kallu Kaalia <kunalmittal.km@outlook.com>
2025-07-14 00:16:15 +0530

    probably the last commit

--- generate_payload.py
+++ generate_payload.py
@@ -20,11 +20,11 @@ import json
 
 # --- Configuration ---
 # This path is relative to where you RUN the script from (the /utils folder)
-TEST_IMAGE_DIR = os.path.join('ai_service', 'test_images', 'prod2')
+TEST_IMAGE_DIR = os.path.join('ai_service', 'test_images', 'prod3')
 OUTPUT_FILE = 'postman_full_inspection_payload.json'
 
 # The reason for the return, as entered by the user.
-RETURN_REASON = "Item did not fit as expected."
+RETURN_REASON = "fitting problem."
 # ---------------------
 
 def encode_image(file_path):
